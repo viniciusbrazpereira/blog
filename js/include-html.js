407475ee4dc8501509fe0fb99deb8ffb763efca6
Vinicius Braz Pereira <vinicius.pereira@rivendel.com.br>
2014-09-02 15:00:59 -0300

    Criando include com jquery.

--- js/include-html.js
+++ js/include-html.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {  	
   	$( "#menu-banner" ).load( "include/menu-banner.html" );
-  	$('a[href="' + this.location.pathname + '"]').parent('li').addClass('active');
+  	$('a[href="index.html"]').addClass('active');
 }); 
