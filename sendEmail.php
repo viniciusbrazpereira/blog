6e54baac7840f27fb5c61423c025341fdc3551e1
Vinicius Braz Pereira <viniciusbrazpereira@gmail.com>
2014-09-06 14:01:43 -0300

    Teste SendEmail.

--- sendEmail.php
+++ sendEmail.php
@@ -1,15 +1,22 @@
+<html>
+<head>
+<title>Sending email using PHP</title>
+</head>
+<body>
 <?php
-$subject = 'testing';
-$email = 'viniciusbrazpereira@gmail.com';
-$message = 'test message';          
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= "From: The test site" . "\r\n";
-
-if (mail($email, $subject, $message, $headers)) {
-  $data['msg']="Message send successfully";
-} 
-else {
-  $data['msg']="Please try again, Message could not be sent!";
-} 
+   $to = "viniciusbrazpereira@gmail.com";
+   $subject = "This is subject";
+   $message = "This is simple text message.";
+   $header = "From:abc@somedomain.com \r\n";
+   $retval = mail ($to,$subject,$message,$header);
+   if( $retval == true )  
+   {
+      echo "Message sent successfully...";
+   }
+   else
+   {
+      echo "Message could not be sent...";
+   }
 ?>
+</body>
+</html>
