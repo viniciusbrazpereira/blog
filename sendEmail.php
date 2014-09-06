b7be705e495572c77bc23c8b94593a4d2a1b5182
Vinicius Braz Pereira <viniciusbrazpereira@gmail.com>
2014-09-06 13:57:08 -0300

    Teste SendEmail.

--- sendEmail.php
+++ sendEmail.php
@@ -1,15 +1,15 @@
 <?php
-//define the receiver of the email
-$to = 'viniciusbrazpereira@gmail.com';
-//define the subject of the email
-$subject = 'Test email';
-//define the message to be sent. 
-$message = "Hello World!\r\nThis is my mail.";
-//define the headers we want passed. 
-$header = "From: viniciusbrazpereira@gmail.com"; // must be a genuine address
-//send the email
-$mail_sent = mail($to, $subject, $message, $header);
-//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed" 
+$subject = 'testing';
+$email = 'viniciusbrazpereira@gmail.com';
+$message = 'test message';          
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= "From: The test site" . "\r\n";
 
-echo $mail_sent ? "Mail sent" : "Mail failed";
+if (mail($email, $subject, $message, $headers)) {
+  $data['msg']="Message send successfully";
+} 
+else {
+  $data['msg']="Please try again, Message could not be sent!";
+} 
 ?>
