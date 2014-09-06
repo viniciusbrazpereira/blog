e5db0c3eef7b5646f7f3d58c543eb3c0bf9729c1
Vinicius Braz Pereira <viniciusbrazpereira@gmail.com>
2014-09-06 13:49:58 -0300

    Teste SendEmail.

--- sendEmail.php
+++ sendEmail.php
@@ -1,7 +1,14 @@
 <?php
+//define the receiver of the email
 $to = 'viniciusbrazpereira@gmail.com';
-$subject = 'This is subject';
-$message = 'This is body of email';
-$from = "From: FirstName LastName <viniciusbrazpereira@gmail.com>";
-mail($to,$subject,$message,$from);
-?> 
+//define the subject of the email
+$subject = 'Test email'; 
+//define the message to be sent. Each line should be separated with \n
+$message = "Hello World!\n\nThis is my first mail."; 
+//define the headers we want passed. Note that they are separated with \r\n
+$headers = "From: viniciusbrazpereira@gmail.com\r\nReply-To: viniciusbrazpereira@gmail.com";
+//send the email
+$mail_sent = @mail( $to, $subject, $message, $headers );
+//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed" 
+echo $mail_sent ? "Mail sent" : "Mail failed";
+?>
