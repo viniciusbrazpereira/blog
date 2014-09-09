b9e936cf02f21f4c8f02a7480455efc2128f1dcc
Vinicius Braz Pereira <viniciusbrazpereira@gmail.com>
2014-09-08 21:38:01 -0300

    Adicionando contato por email.

--- js/include-html.js
+++ js/include-html.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {  	
   	$( "#menu-banner" ).load( "include/menu-banner.html" );
   	$( "#artigos-social-media" ).load( "include/artigos-social-media.html" );
+  	$( "#footer" ).load( "include/footer.html" );
 }); 
