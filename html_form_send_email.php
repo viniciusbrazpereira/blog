dca96dd2b9ee2f70b2322f53a9a833617f88a4d6
Vinicius Braz Pereira <vinicius.pereira@rivendel.com.br>
2014-09-12 10:36:27 -0300

    Atualizando send email.8

--- html_form_send_email.php
+++ html_form_send_email.php
@@ -1,47 +1,5 @@
 <?php
 
-require_once('class.phpmailer.php');
-//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
-
-$mail             = new PHPMailer();
-
-$body             = "gdssdh";
-//$body             = eregi_replace("[\]",'',$body);
-
-$mail->IsSMTP(); // telling the class to use SMTP
-$mail->Host       = "ssl://smtp.gmail.com"; // SMTP server
-$mail->SMTPDebug  = 1;                     // enables SMTP debug information (for testing)
-                                           // 1 = errors and messages
-                                           // 2 = messages only
-$mail->SMTPAuth   = true;                  // enable SMTP authentication
-$mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
-$mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
-$mail->Port       = 587;                   // set the SMTP port for the GMAIL server
-$mail->Username   = "viniciusbrazpereira@gmail.com";  // GMAIL username
-$mail->Password   = "Antonio@";            // GMAIL password
-
-$mail->SetFrom('viniciusbrazpereira@gmail.com', 'PRSPS');
-
-//$mail->AddReplyTo("user2@gmail.com', 'First Last");
-
-$mail->Subject    = "PRSPS password";
-
-//$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-
-$mail->MsgHTML($body);
-
-$address = "viniciusbrazpereira@gmail.com";
-$mail->AddAddress($address, "user2");
-
-//$mail->AddAttachment("images/phpmailer.gif");      // attachment
-//$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
-
-if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-  echo "Message sent!";
-}
-
 echo "PHPMailer";
 
 ?>
