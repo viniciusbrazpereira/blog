8bc92fb005664a6f4c020de2d8ed14518c600e66
Vinicius Braz Pereira <vinicius.pereira@rivendel.com.br>
2014-09-12 10:08:29 -0300

    Atualizando send email.5

--- html_form_send_email.php
+++ html_form_send_email.php
@@ -1,43 +1,64 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>PHPMailer - sendmail test</title>
-</head>
-<body>
 <?php
+ 
+// Inclui o arquivo class.phpmailer.php localizado na pasta phpmailer
 require("PHPMailer/class.phpmailer.php");
-
-
-//Create a new PHPMailer instance
+ 
+// Inicia a classe PHPMailer
 $mail = new PHPMailer();
+ 
+// Define os dados do servidor e tipo de conexão
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 $mail->IsSMTP(); // Define que a mensagem será SMTP
-$mail->Host = "smtp.dominio.net"; // Endereço do servidor SMTP
-
+$mail->Host = "smtp.gmail.com"; // Endereço do servidor SMTP (caso queira utilizar a autenticação, utilize o host smtp.seudomínio.com.br)
+$mail->SMTPAuth = true; // Usar autenticação SMTP (obrigatório para smtp.seudomínio.com.br)
+$mail->Username = 'viniciusbrazpereira@gmail.com'; // Usuário do servidor SMTP (endereço de email)
+$mail->Password = 'Antonio@'; // Senha do servidor SMTP (senha do email usado)
+ 
+// Define o remetente
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 $mail->From = "viniciusbrazpereira@gmail.com"; // Seu e-mail
-
-$mail->AddAddress('viniciusbrazpereira@gmail.com', 'Fulano da Silva');
-
+$mail->Sender = "viniciusbrazpereira@gmail.com"; // Seu e-mail
+$mail->FromName = "Locaweb"; // Seu nome
+ 
+// Define os destinatário(s)
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+$mail->AddAddress('viniciusbrazpereira@gmail.com', 'Teste Locaweb');
+//$mail->AddCC('ciclano@site.net', 'Ciclano'); // Copia
+//$mail->AddBCC('fulano@dominio.com.br', 'Fulano da Silva'); // Cópia Oculta
+ 
+// Define os dados técnicos da Mensagem
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 $mail->IsHTML(true); // Define que o e-mail será enviado como HTML
-
+//$mail->CharSet = 'iso-8859-1'; // Charset da mensagem (opcional)
+ 
+// Define a mensagem (Texto e Assunto)
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 $mail->Subject  = "Mensagem Teste"; // Assunto da mensagem
-$mail->Body = "Este é o corpo da mensagem de teste, em <b>HTML</b>! <br /> <img src="http://i2.wp.com/blog.thiagobelem.net/wp-includes/images/smilies/icon_smile.gif?w=625" alt=":)" class="wp-smiley" width="15" height="15"> ";
-$mail->AltBody = "Este é o corpo da mensagem de teste, em Texto Plano! \r\n <img src="http://i2.wp.com/blog.thiagobelem.net/wp-includes/images/smilies/icon_smile.gif?w=625" alt=":)" class="wp-smiley" width="15" height="15"> ";
-
+$mail->Body = 'Este é o corpo da mensagem de teste, em HTML! 
+ <IMG src="http://seudomínio.com.br/imagem.jpg" alt=":)"   class="wp-smiley"> ';
+$mail->AltBody = 'Este é o corpo da mensagem de teste, em Texto Plano! \r\n 
+<IMG src="http://seudomínio.com.br/imagem.jpg" alt=":)"  class="wp-smiley"> ';
+ 
+// Define os anexos (opcional)
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//$mail->AddAttachment("/home/login/documento.pdf", "novo_nome.pdf");  // Insere um anexo
+ 
+// Envia o e-mail
 $enviado = $mail->Send();
-
+ 
 // Limpa os destinatários e os anexos
 $mail->ClearAllRecipients();
-$mail->ClearAttachments(); 
+$mail->ClearAttachments();
+ 
 // Exibe uma mensagem de resultado
 if ($enviado) {
-	echo "E-mail enviado com sucesso!";
+echo "E-mail enviado com sucesso!";
 } else {
-	echo "Não foi possível enviar o e-mail.<br /><br />";
-	echo "<b>Informações do erro:</b> <br />" . $mail->ErrorInfo;
+echo "Não foi possível enviar o e-mail.
+ 
+";
+echo "Informações do erro: 
+" . $mail->ErrorInfo;
 }
-
+ 
 ?>
-</body>
-</html>
-
