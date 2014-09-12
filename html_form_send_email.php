52bb6ecdc950da6ac6118c8e93c9ff123b90b46f
Vinicius Braz Pereira <vinicius.pereira@rivendel.com.br>
2014-09-12 10:49:39 -0300

    Atualizando send email.10

--- html_form_send_email.php
+++ html_form_send_email.php
@@ -1,5 +1,36 @@
 <?php
 
+require_once('PHPMailer/class.phpmailer.php');
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->CharSet="UTF-8";
+$mail->SMTPSecure = 'tls';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->Username = 'viniciusbrazpereira@gmail.com';
+$mail->Password = 'Antonio@';
+$mail->SMTPAuth = true;
+
+$mail->From = 'viniciusbrazpereira@gmail.com';
+$mail->FromName = 'Mohammad Masoudian';
+$mail->AddAddress('viniciusbrazpereira@gmail.com');
+$mail->AddReplyTo('viniciusbrazpereira@gmail.com', 'Information');
+
+$mail->IsHTML(true);
+$mail->Subject    = "PHPMailer Test Subject via Sendmail, basic";
+$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!";
+$mail->Body    = "Hello";
+
+if(!$mail->Send())
+{
+  echo "Mailer Error: " . $mail->ErrorInfo;
+}
+else
+{
+  echo "Message sent!";
+}
+
 phpinfo() ;
 
 ?>
