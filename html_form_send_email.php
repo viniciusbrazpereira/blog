f0ee11399a25279fbe7492707bc11db61fb13b51
Vinicius Braz Pereira <vinicius.pereira@rivendel.com.br>
2014-09-12 10:12:48 -0300

    Atualizando send email.6

--- html_form_send_email.php
+++ html_form_send_email.php
@@ -1,64 +1,45 @@
 <?php
- 
-// Inclui o arquivo class.phpmailer.php localizado na pasta phpmailer
-require("PHPMailer/class.phpmailer.php");
- 
-// Inicia a classe PHPMailer
-$mail = new PHPMailer();
- 
-// Define os dados do servidor e tipo de conexão
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->IsSMTP(); // Define que a mensagem será SMTP
-$mail->Host = "smtp.gmail.com"; // Endereço do servidor SMTP (caso queira utilizar a autenticação, utilize o host smtp.seudomínio.com.br)
-$mail->SMTPAuth = true; // Usar autenticação SMTP (obrigatório para smtp.seudomínio.com.br)
-$mail->Username = 'viniciusbrazpereira@gmail.com'; // Usuário do servidor SMTP (endereço de email)
-$mail->Password = 'Antonio@'; // Senha do servidor SMTP (senha do email usado)
- 
-// Define o remetente
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->From = "viniciusbrazpereira@gmail.com"; // Seu e-mail
-$mail->Sender = "viniciusbrazpereira@gmail.com"; // Seu e-mail
-$mail->FromName = "Locaweb"; // Seu nome
- 
-// Define os destinatário(s)
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->AddAddress('viniciusbrazpereira@gmail.com', 'Teste Locaweb');
-//$mail->AddCC('ciclano@site.net', 'Ciclano'); // Copia
-//$mail->AddBCC('fulano@dominio.com.br', 'Fulano da Silva'); // Cópia Oculta
- 
-// Define os dados técnicos da Mensagem
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->IsHTML(true); // Define que o e-mail será enviado como HTML
-//$mail->CharSet = 'iso-8859-1'; // Charset da mensagem (opcional)
- 
-// Define a mensagem (Texto e Assunto)
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->Subject  = "Mensagem Teste"; // Assunto da mensagem
-$mail->Body = 'Este é o corpo da mensagem de teste, em HTML! 
- <IMG src="http://seudomínio.com.br/imagem.jpg" alt=":)"   class="wp-smiley"> ';
-$mail->AltBody = 'Este é o corpo da mensagem de teste, em Texto Plano! \r\n 
-<IMG src="http://seudomínio.com.br/imagem.jpg" alt=":)"  class="wp-smiley"> ';
- 
-// Define os anexos (opcional)
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-//$mail->AddAttachment("/home/login/documento.pdf", "novo_nome.pdf");  // Insere um anexo
- 
-// Envia o e-mail
-$enviado = $mail->Send();
- 
-// Limpa os destinatários e os anexos
-$mail->ClearAllRecipients();
-$mail->ClearAttachments();
- 
-// Exibe uma mensagem de resultado
-if ($enviado) {
-echo "E-mail enviado com sucesso!";
+
+require_once('PHPMailer/class.phpmailer.php');
+//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
+
+$mail             = new PHPMailer();
+
+$body             = "gdssdh";
+//$body             = eregi_replace("[\]",'',$body);
+
+$mail->IsSMTP(); // telling the class to use SMTP
+$mail->Host       = "ssl://smtp.gmail.com"; // SMTP server
+$mail->SMTPDebug  = 1;                     // enables SMTP debug information (for testing)
+                                           // 1 = errors and messages
+                                           // 2 = messages only
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
+$mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
+$mail->Port       = 587;                   // set the SMTP port for the GMAIL server
+$mail->Username   = "viniciusbrazpereira@gmail.com";  // GMAIL username
+$mail->Password   = "Antonio@";            // GMAIL password
+
+$mail->SetFrom('viniciusbrazpereira@gmail.com', 'PRSPS');
+
+//$mail->AddReplyTo("user2@gmail.com', 'First Last");
+
+$mail->Subject    = "PRSPS password";
+
+//$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+
+$mail->MsgHTML($body);
+
+$address = "viniciusbrazpereira@gmail.com";
+$mail->AddAddress($address, "user2");
+
+//$mail->AddAttachment("images/phpmailer.gif");      // attachment
+//$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
+
+if(!$mail->Send()) {
+  echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
-echo "Não foi possível enviar o e-mail.
- 
-";
-echo "Informações do erro: 
-" . $mail->ErrorInfo;
+  echo "Message sent!";
 }
- 
+
 ?>
