bbe859b6dc9322b5a23c267adef1ee06ccc2017a
Vinicius Braz Pereira <vinicius.pereira@rivendel.com.br>
2014-09-12 09:45:10 -0300

    Atualizando send email.2

--- html_form_send_email.php
+++ html_form_send_email.php
@@ -20,9 +20,6 @@ $mail->addReplyTo('viniciusbrazpereira@gmail.com', 'First Last');
 $mail->addAddress('viniciusbrazpereira@gmail.com', 'John Doe');
 //Set the subject line
 $mail->Subject = 'PHPMailer sendmail test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
 //Replace the plain text body with one created manually
 $mail->AltBody = 'This is a plain-text message body';
 
