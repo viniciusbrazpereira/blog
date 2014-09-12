3ba36ed731e1c9db1f3eefa966890572ee3286b4
Vinicius Braz Pereira <vinicius.pereira@rivendel.com.br>
2014-09-12 14:10:27 -0300

    Adicionando PHPMailer.10

--- html_form_send_email.php
+++ html_form_send_email.php
@@ -1,11 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>PHPMailer - SMTP test</title>
+</head>
+<body>
 <?php
 
 require 'PHPMailer/PHPMailerAutoload.php';
 
 //Create a new PHPMailer instance
 $mail = new PHPMailer();
-// Set PHPMailer to use the sendmail transport
-$mail->isSendmail();
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+//Set the hostname of the mail server
+$mail->Host = "smtp.gamil.com";
+//Set the SMTP port number - likely to be 25, 465 or 587
+$mail->Port = 587;
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication
+$mail->Username = "viniciusbrazpereira@gmail.com";
+//Password to use for SMTP authentication
+$mail->Password = "Antonio@";
 //Set who the message is to be sent from
 $mail->setFrom('viniciusbrazpereira@gmail.com', 'First Last');
 //Set an alternative reply-to address
@@ -13,11 +37,10 @@ $mail->addReplyTo('viniciusbrazpereira@gmail.com', 'First Last');
 //Set who the message is to be sent to
 $mail->addAddress('viniciusbrazpereira@gmail.com', 'John Doe');
 //Set the subject line
-$mail->Subject = 'PHPMailer sendmail test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
+$mail->Subject = 'PHPMailer SMTP test';
 //Replace the plain text body with one created manually
 $mail->AltBody = 'This is a plain-text message body';
-
+// body
 $mail->Body    = "Hello";
 
 //send the message, check for errors
@@ -26,5 +49,6 @@ if (!$mail->send()) {
 } else {
     echo "Message sent!";
 }
-
 ?>
+</body>
+</html>
