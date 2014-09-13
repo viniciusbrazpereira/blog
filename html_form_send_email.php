93b68d834a35768b1ee46c4793ebde6b467a4c39
Vinicius Braz Pereira <viniciusbrazpereira@gmail.com>
2014-09-13 14:51:34 -0300

    Test send email with PHPMailer. 4

--- html_form_send_email.php
+++ html_form_send_email.php
@@ -21,11 +21,11 @@ $mail->SMTPAuth = true;
 // 2 = client and server messages
 $mail->SMTPDebug = 4;
 //Set the hostname of the mail server
-$mail->Host = "smtp.gmail.com";
+$mail->Host = "ssl://smtp.gmail.com";
 //Set the SMTP port number - likely to be 25, 465 or 587
-$mail->Port = 25;
+$mail->Port = 465;
 // SMTPSecure
-//$mail->SMTPSecure = "tls";
+//$mail->SMTPSecure = "ssl";
 // CharSet
 $mail->CharSet = 'UTF-8';
 //Username to use for SMTP authentication
