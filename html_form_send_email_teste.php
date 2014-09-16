606b385147ad6c6f84e13d478f46ee436749aa75
Vinicius Braz Pereira <viniciusbrazpereira@gmail.com>
2014-09-16 20:09:45 -0300

    Teste email send.

---
+++ html_form_send_email_teste.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>PHPMailer - SMTP test</title>
+</head>
+<body>
+<?php
+
+require 'PHPMailer/PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Whether to use SMTP authentication
+//$mail->SMTPAuth = true;
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 4;
+//Set the hostname of the mail server
+//$mail->Host = "smtp.gmail.com";
+//Set the SMTP port number - likely to be 25, 465 or 587
+//$mail->Port = 587;
+// SMTPSecure
+//$mail->SMTPSecure = "tls";
+// CharSet
+//$mail->CharSet = 'UTF-8';
+//Username to use for SMTP authentication
+//$mail->Username = "viniciusbrazpereira@gmail.com";
+//Set who the message is to be sent from
+$mail->setFrom('viniciusbrazpereira@gmail.com', 'First Last');
+//Set an alternative reply-to address
+$mail->addReplyTo('viniciusbrazpereira@gmail.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('viniciusbrazpereira@gmail.com', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer SMTP test';
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+// body
+$mail->Body    = "Hello";
+// Set word wrap to 50 characters
+$mail->WordWrap = 50;                                 
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+?>
+</body>
+</html>
