0f3b3fd55d36ddfe5d56ef24332c1785afe76f7e
Vinicius Braz Pereira <viniciusbrazpereira@gmail.com>
2014-09-16 20:18:43 -0300

    Teste email send. 3

--- html_form_send_email_teste.php
+++ html_form_send_email_teste.php
@@ -19,7 +19,7 @@ $mail->isSMTP();
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
-$mail->SMTPDebug = 3;
+$mail->SMTPDebug = 2;
 //Set the hostname of the mail server
 //$mail->Host = "smtp.gmail.com";
 //Set the SMTP port number - likely to be 25, 465 or 587
