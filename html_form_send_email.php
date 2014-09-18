179d3973f122d31f7ba3c56f5e428b2df76d6d0a
Vinicius Braz Pereira <vinicius.pereira@rivendel.com.br>
2014-09-18 10:45:18 -0300

    Test Send Email. 2

--- html_form_send_email.php
+++ html_form_send_email.php
@@ -7,52 +7,88 @@
 <body>
 <?php
 
-require 'PHPMailer/PHPMailerAutoload.php';
+if(isset($_POST['email'])) {
 
-//Create a new PHPMailer instance
-$mail = new PHPMailer();
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
-//Set the hostname of the mail server
-$mail->Host = "smtp.gmail.com";
-//Set the SMTP port number - likely to be 25, 465 or 587
-$mail->Port = 465;
-// SMTPSecure
-$mail->SMTPSecure = "ssl";
-// CharSet
-//$mail->CharSet = 'UTF-8';
-//Username to use for SMTP authentication
-$mail->Username = "viniciusbrazpereira@gmail.com";
-//Password to use for SMTP authentication
-$mail->Password = "Antonio@";
-//Set who the message is to be sent from
-$mail->setFrom('viniciusbrazpereira@gmail.com', 'First Last');
-//Set an alternative reply-to address
-$mail->addReplyTo('viniciusbrazpereira@gmail.com', 'First Last');
-//Set who the message is to be sent to
-$mail->addAddress('viniciusbrazpereira@gmail.com', 'John Doe');
-//Set the subject line
-$mail->Subject = 'PHPMailer SMTP test Email';
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-// body
-$mail->Body    = "Hello";
-// Set word wrap to 50 characters
-$mail->WordWrap = 50;                                 
+	require 'PHPMailer/PHPMailerAutoload.php';
+	$mail = new PHPMailer();
+	$mail->isSMTP();
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 465;
+	$mail->SMTPSecure = "ssl";
+	$mail->CharSet = 'UTF-8';
+	//Username to use for SMTP authentication
+	$mail->Username = "viniciusbrazpereira@gmail.com";
+	//Password to use for SMTP authentication
+	$mail->Password = "Antonio@";
+	//Set who the message is to be sent from
+	$mail->setFrom('viniciusbrazpereira@gmail.com', 'Vinicius Braz Pereira');
+	//Set an alternative reply-to address
+	$mail->addReplyTo('viniciusbrazpereira@gmail.com', 'Vinicius Braz Pereira');
 
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
-?>
+	function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+     
+    // validation expected data exists
+    if(!isset($_POST['first_name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+
+    $first_name = $_POST['first_name']; // required
+    $email_from = $_POST['email']; // required
+    $comments = $_POST['comments']; // required
+     
+    $error_message = "";
+    
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+	if(!preg_match($email_exp,$email_from)) {
+		$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+	}
+	
+	$string_exp = "/^[A-Za-z .'-]+$/";
+	if(!preg_match($string_exp,$first_name)) {
+		$error_message .= 'The First Name you entered does not appear to be valid.<br />';
+	}
+
+	if(strlen($comments) < 2) {
+		$error_message .= 'The Comments you entered do not appear to be valid.<br />';
+	}
+
+	if(strlen($error_message) > 0) {
+		died($error_message);
+	}
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+    //Set who the message is to be sent to
+	$mail->addAddress(''.clean_string($email_from), ''.clean_string($first_name));
+	//Set the subject line
+	$mail->Subject = 'Website www.viniciusbrazpereira.com.br contact you.';
+	//Replace the plain text body with one created manually
+	$mail->AltBody = 'This is a plain-text message body';
+    // body
+	$mail->Body    = "Comments: ".clean_string($comments)."\n";
+	// Set word wrap to 50 characters
+	//$mail->WordWrap = 50;   
+
+	//send the message, check for errors
+	if ($mail->send()) {
+	   header("Location: http://viniciusbrazpereira.com.br/contato.html");
+	} else {
+	    echo "Mailer Error: " . $mail->ErrorInfo;
+	}
+
+	exit;
+}?>
 </body>
 </html>
